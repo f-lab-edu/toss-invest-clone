79959e6cba401256bfa4b2e21803228f4858ac3b
jihyeon-songb <dhqtl1@naver.com>
2025-09-10 22:46:33 +0900

    style: main.js 불필요한 콤마 제거

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter(routes);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />,
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>,
 );
