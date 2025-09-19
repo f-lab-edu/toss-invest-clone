0627cda164b0122fdd311e3de1500860dc3f76fa
jihyeon-songb <dhqtl1@naver.com>
2025-09-19 13:26:22 +0900

    chore: @tanstack/react-query 라이브러리 설치 및 QueryClientProvider 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./routes.tsx";
 import "./index.css";
 import { AuthProvider } from "@/components/common/AuthProvider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter(routes);
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />,
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />,
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
