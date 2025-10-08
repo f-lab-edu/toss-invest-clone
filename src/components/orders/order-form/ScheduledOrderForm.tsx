d72908d11626a737ab980e933b5abe51e407b002
jihyeon-songb <dhqtl1@naver.com>
2025-10-08 15:37:01 +0900

    refactor: orderFormType 기준으로 컴포넌트 분리

---
+++ src/components/orders/order-form/ScheduledOrderForm.tsx
@@ -0,0 +1,6 @@
+import type { FC } from "react";
+
+const ScheduledOrderForm: FC = () => {
+  return null;
+};
+export default ScheduledOrderForm;
---
+++ src/components/orders/order-form/SellOrderForm.tsx
@@ -0,0 +1,6 @@
+import type { FC } from "react";
+
+const SellOrderForm: FC = () => {
+  return null;
+};
+export default SellOrderForm;
